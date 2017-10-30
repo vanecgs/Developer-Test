b24284c01aa7269d4e0355d436d006ecec0e86de
Vanessa Gutierrez <vgutierrez@summasolutions.net>
2017-10-30 11:15:19 -0300

    Task 3

--- test.js
+++ test.js
@@ -23,3 +23,27 @@ function reformat(phrase) {
     phrase = phrase.replace( /[aeiou]/g, '' ).toLowerCase();
     return phrase.charAt(0).toUpperCase() + phrase.slice(1);
 }
+
+function next_binary_number(number) {
+    var lastZero = 0;
+
+    for(var i = 0; i < number.length; i++) {
+        if(!number[i]) {
+            lastZero = i;
+        }
+    }
+
+    for(i = lastZero; i < number.length; i++) {
+        if(!number[i]) {
+            number[i] = 1;
+        } else {
+            number[i] = 0;
+        }
+    }
+
+    if(!lastZero) {
+        number.unshift(1);
+    }
+
+    return number;
+}
