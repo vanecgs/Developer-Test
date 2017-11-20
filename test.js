0dd0bb5e20d1dcc5fd36610de7094e7b1868b42a
Vanessa Gutierrez <vgutierrez@summasolutions.net>
2017-11-20 12:58:26 -0300

    Another way to solve exercise #1 - in case it was an array you needed and not a string

--- test.js
+++ test.js
@@ -1,19 +1,12 @@
 function repeat(numbers) {
     var repeatTimes = 3,
-        repeatResult = '[';
+        result = [];
 
     if(numbers && numbers.length) {
         for(var i = 0; i < repeatTimes; i++) {
-            for(var j = 0; j < numbers.length; j++) {
-                repeatResult += numbers[j];
-                repeatResult += ',';
-            }
+            result = result.concat(numbers);
         }
-
-        repeatResult = repeatResult.substring(0, repeatResult.length - 1);
-        repeatResult += ']';
-
-        return repeatResult;
+        return result;
     }
 
     return false;
